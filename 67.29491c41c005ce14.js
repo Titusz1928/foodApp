f13b4f8621740881d0e76aac2c91d78e010bd000
Titusz <titusz.boros@student.upt.ro>
2024-07-13 12:25:37 +0300

    Auto-generated commit

---
+++ 67.29491c41c005ce14.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfoodapp=self.webpackChunkfoodapp||[]).push([[67],{67:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
